c09774a21630589bce91682ff61f308213f20cdb
erinlhiggins <elh76@pitt.edu>
2020-04-28 17:13:17 -0400

    Add files via upload

---
+++ MTG Image Recognition/MTG Image Recognition/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  MTG Image Recognition
+//
+//  Created by Erin Higgins on 7/31/18.
+//  Copyright © 2018 Erin Higgins. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+   
+    let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Label"
+        label.font = label.font.withSize(30)
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCaptureSession()
+        
+        view.addSubview(label)
+        setupLabel()
+    }
+    
+    
+    
+    func setupCaptureSession() {
+        
+        let captureSession = AVCaptureSession()
+        
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        do {
+            if let captureDevice = availableDevices.first {
+                captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(captureOutput)
+        
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            
+            DispatchQueue.main.async(execute: {
+                self.label.text = "\(Observation.identifier)"
+                print(Observation.confidence)
+            })
+        }
+         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+    func setupLabel() {
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+}
